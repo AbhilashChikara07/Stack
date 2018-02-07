adbcb551d31393e6d2141b41cdad98baf88b8f4c
Abhilash Chikara <abhilash.chikara@fastfox.com>
2018-02-07 14:38:24 +0530

    done reverse stack

--- app/src/main/java/com/example/chikara/stack/MainActivity.java
+++ app/src/main/java/com/example/chikara/stack/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,
-                        ReverseStack.class);
+                        ShortStackUsingTempStack.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/chikara/stack/ShortStackUsingTempStack.kt
@@ -0,0 +1,48 @@
+package com.example.chikara.stack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import java.util.*
+
+/**
+ * Created by chikara on 2/7/18.
+ */
+
+class ShortStackUsingTempStack : AppCompatActivity() {
+
+    private var stackObj: Stack<Int>? = Stack()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.stack_layout)
+        stackObj!!.push(2)
+        stackObj!!.push(4)
+        stackObj!!.push(1)
+        stackObj!!.push(0)
+
+        val mStack = shortStack()
+        mStack.pop()
+        mStack.pop()
+        mStack.pop()
+
+        Log.e("top element :- ", "" + mStack.peek())
+    }
+
+    private fun shortStack(): Stack<Int> {
+        val tempStack: Stack<Int>? = Stack()
+        while (stackObj!!.size > 0) {
+            val value = stackObj!!.pop()
+            if (tempStack!!.isEmpty()) {
+                tempStack.push(value)
+            } else {
+                while (tempStack.peek() > value) {
+                    stackObj!!.push(tempStack.pop())
+                }
+                tempStack.push(value)
+            }
+        }
+        return tempStack!!
+    }
+
+}
